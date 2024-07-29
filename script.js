45db9bbdc13cba586f1a3f60cd418887925acbeb
Yashjain1623 <157748380+Yashjain1623@users.noreply.github.com>
2024-07-29 08:14:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
 //complete this code
-class Person {}
+class Person {
+	 constructor(name, age) {
+        this.name = name; // Public property
+        this.age = age;   // Public property
+    }
 
-class Student extends Person {}
+    // Getter for name
+    getName() {
+        return this.name;
+    }
 
-class Teacher extends Person {}
+    // Setter for age
+    setAge(newAge) {
+        if (newAge > 0) {
+            this.age = newAge;
+        } else {
+            console.log('Age must be a positive number.');
+        }
+    }
+}
+}
+
+class Student extends Person {
+	 study() {
+        console.log(`${this.getName()} is studying.`);
+    }
+}
+
+class Teacher extends Person {teach() {
+        console.log(`${this.getName()} is teaching.`);
+    }}
 
 // Do not change the code below this line
 window.Person = Person;
