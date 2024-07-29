2ad66eb9ca89f17440e9ca3a15ecd17abc091684
Yashjain1623 <157748380+Yashjain1623@users.noreply.github.com>
2024-07-29 08:23:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -38,10 +38,10 @@ class Teacher extends Person {
 
 // Example usage
 const person = new Person("John", 25);
-console.log(person.name); // Output: John
+console.log(Person.name); // Output: John
 
 person.age = 30;
-console.log(person.age); // Output: 30
+console.log(Person.age); // Output: 30
 
 const student = new Student("Alice", 22);
 student.study(); // Output: Alice is studying
